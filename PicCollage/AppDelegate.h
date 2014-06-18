2d886b4df0879552d73076b0bfa925a793646f4f
iOsNeelShah <neels@SA-MAC-007.local>
2014-06-18 16:45:20 +0530

    first commit
    doing first commit

---
+++ PicCollage/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  PicCollage
+//
+//  Created by Neel Shah on 18/06/14.
+//  Copyright (c) 2014 com.solutionanalysts. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ PicCollageTests/PicCollageTests.m
@@ -0,0 +1,34 @@
+//
+//  PicCollageTests.m
+//  PicCollageTests
+//
+//  Created by Neel Shah on 18/06/14.
+//  Copyright (c) 2014 com.solutionanalysts. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface PicCollageTests : XCTestCase
+
+@end
+
+@implementation PicCollageTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
